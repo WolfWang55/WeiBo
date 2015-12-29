001988f5696911243e9c5e739e1019786894ccb3
LG <thelast_wolf@163.com>
2015-12-29 09:34:14 +0800

    Initial Commit

---
+++ Weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weibo
+//
+//  Created by LG on 15/12/29.
+//  Copyright (c) 2015年 LG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weibo
+//
+//  Created by LG on 15/12/29.
+//  Copyright (c) 2015年 LG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
